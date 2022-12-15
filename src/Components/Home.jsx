4d9366c095df55305c662e332b605b86a016b83e
Amir <amirbhai432@gmail.com>
2022-12-16 02:18:03 +0530

    Home Page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import me from "../Assets/AMIR3.JPG";
+
+const Home = () => {
+  return (
+    <div className="">
+      <div></div>
+      <div className="d-flex justify-content-center align-item-center">
+        <div className="py-5">
+          <img
+            className="border border-danger rounded-3"
+            src={me}
+            alt=""
+          />
+        </div>
+        <div className="p-5 fs-5">
+          <h2 className="fs-1 fst-italic">Hi , there !</h2>
+          <div>
+            <span className="">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+            </span>
+          </div>
+          <div>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos, enim! Exercitationem
+            </span>
+          </div>
+          <div>
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam,
+              neque.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-center fs-5">
+        <h2 className="px-2">My Career So For </h2>
+        <div>
+          <span className="text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea possimus
+            voluptatum?
+          </span>
+          <div>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, ea.
+            </span>
+          </div>
+          <div>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing</span>
+          </div>
+          <div>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing</span>
+          </div>
+          <div>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing</span>
+          </div>
+        </div>
+        <div className="m-1">
+          <div
+            className="badge text-dark text-wrap border border-dark m-1"
+            style={{ width: 5 + "rem", height: 2 + "rem " }}
+          >
+            React.js
+          </div>
+          <div
+            className="badge text-dark text-wrap border border-dark m-1"
+            style={{ width: 5 + "rem", height: 2 + "rem " }}
+          >
+            React.js
+          </div>
+          <div
+            className="badge text-dark text-wrap border border-dark m-1"
+            style={{ width: 5 + "rem", height: 2 + "rem " }}
+          >
+            React.js
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { VscAccount } from "react-icons/vsc";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-black">
+      <div className="container-fluid">
+        <a className="navbar-brand text-white " href="#home">
+          <i className="mx-2">
+            <VscAccount />
+          </i>
+          Amir
+        </a>
+        <div className="d-flex p-2 ">
+          <div className="mx-3 text-light">
+            <a className="fw-500 text-light" href="#github">
+              GitHub
+            </a>
+          </div>
+          <div className="mx-3">
+            <a className="text-light" href="linkedIn">
+              LinkedIn
+            </a>
+          </div>
+          <div className="mx-3">
+            <a className="text-light" href="#contact">
+              Contact
+            </a>
+          </div>
+
+          <div className="mx-2">
+            <button className="btn shadow-sm bg-body rounded">view Work</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
